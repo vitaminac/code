c6bd4a91bf36a80f405b0891c64d70ec8ba50520
vitam <psyviccccc@gmail.com>
2017-11-19 19:01:02 +0100

    symbolic multiply rough tests passed

--- src/test/java/symbolic/operator/arithmetic/MultiplyTest.java
+++ src/test/java/symbolic/operator/arithmetic/MultiplyTest.java
@@ -68,18 +68,55 @@ public class MultiplyTest {
     public void greatestCommonFactor() {
         final String sameGCF = "the Greatest common divisor of two same expression is themselves";
         assertEquals(sameGCF, new Multiply(new Constant(2), new Symbol("x")), this.twoTimeX.greatestCommonFactor(this.twoTimeX));
-        assertEquals(sameGCF, this.twoTimeX.greatestCommonFactor(this.twoTimeX), this.twoTimeX);
-        assertEquals(sameGCF, this.xTimeTwo.greatestCommonFactor(this.xTimeTwo), this.xTimeTwo);
-        assertEquals(sameGCF, this.threeTimeX.greatestCommonFactor(this.threeTimeX), this.threeTimeX);
-        assertEquals(sameGCF, this.twoTimeY.greatestCommonFactor(this.twoTimeY), this.twoTimeY);
-        assertEquals(sameGCF, this.threeTimeY.greatestCommonFactor(this.threeTimeY), this.threeTimeY);
+        assertEquals(sameGCF, this.twoTimeX, this.twoTimeX.greatestCommonFactor(this.twoTimeX));
+        assertEquals(sameGCF, this.twoTimeX, this.twoTimeX.greatestCommonFactor(this.xTimeTwo));
+        assertEquals(sameGCF, this.xTimeTwo, this.xTimeTwo.greatestCommonFactor(this.xTimeTwo));
+        assertEquals(sameGCF, this.xTimeTwo, this.xTimeTwo.greatestCommonFactor(this.twoTimeX));
+        assertEquals(sameGCF, this.threeTimeX, this.threeTimeX.greatestCommonFactor(this.threeTimeX));
+        assertEquals(sameGCF, this.twoTimeY, this.twoTimeY.greatestCommonFactor(this.twoTimeY));
+        assertEquals(sameGCF, this.threeTimeY, this.threeTimeY.greatestCommonFactor(this.threeTimeY));
+        assertEquals(sameGCF, new Constant(4), this.twoByTwo.greatestCommonFactor(this.twoByTwo));
+        assertEquals(sameGCF, new Constant(0), this.zeroTimeX.greatestCommonFactor(this.zeroTimeX));
 
-        assertEquals("the MCF is 2, x cannot be factored", new Constant(2), this.twoTimeX.greatestCommonFactor(this.twoByTwo));
-        assertEquals(this.twoTimeX.greatestCommonFactor(this.twoTimeY), new Constant(2));
+        String isX = "The common factor is x";
+        assertEquals(isX, new Symbol("x"), this.twoTimeX.greatestCommonFactor(this.threeTimeX));
+        assertEquals(isX, new Symbol("x"), this.xTimeTwo.greatestCommonFactor(this.threeTimeX));
+        assertEquals(isX, new Symbol("x"), this.threeTimeX.greatestCommonFactor(this.twoTimeX));
+        assertEquals(isX, new Symbol("x"), this.threeTimeX.greatestCommonFactor(this.xTimeTwo));
 
-        assertEquals(this.twoTimeX.greatestCommonFactor(this.threeTimeX), new Symbol("x"));
-
+        String isY = "The common factor is y";
         assertEquals(this.twoTimeY.greatestCommonFactor(this.threeTimeY), new Symbol("y"));
+        assertEquals(this.threeTimeY.greatestCommonFactor(this.twoTimeY), new Symbol("y"));
+
+
+        String noCommonFactor = "they have not a common factor, the result is one";
+        assertEquals(noCommonFactor, new Constant(1), this.twoTimeX.greatestCommonFactor(this.threeTimeY));
+        assertEquals(noCommonFactor, new Constant(1), this.xTimeTwo.greatestCommonFactor(this.threeTimeY));
+        assertEquals(noCommonFactor, new Constant(1), this.threeTimeX.greatestCommonFactor(this.twoTimeY));
+        assertEquals(noCommonFactor, new Constant(1), this.threeTimeX.greatestCommonFactor(this.twoByTwo));
+        assertEquals(noCommonFactor, new Constant(1), this.twoTimeY.greatestCommonFactor(this.threeTimeX));
+        assertEquals(noCommonFactor, new Constant(1), this.threeTimeY.greatestCommonFactor(this.twoTimeX));
+        assertEquals(noCommonFactor, new Constant(1), this.threeTimeY.greatestCommonFactor(this.xTimeTwo));
+        assertEquals(noCommonFactor, new Constant(1), this.threeTimeY.greatestCommonFactor(this.twoByTwo));
+        assertEquals(noCommonFactor, new Constant(1), this.twoByTwo.greatestCommonFactor(this.threeTimeX));
+        assertEquals(noCommonFactor, new Constant(1), this.twoByTwo.greatestCommonFactor(this.threeTimeY));
+
+
+        String isTwo = "the MCF is constant two";
+        assertEquals(isTwo, new Constant(2), this.twoTimeX.greatestCommonFactor(this.twoTimeY));
+        assertEquals(isTwo, new Constant(2), this.xTimeTwo.greatestCommonFactor(this.twoTimeY));
+        assertEquals(isTwo, new Constant(2), this.twoTimeX.greatestCommonFactor(this.twoByTwo));
+        assertEquals(isTwo, new Constant(2), this.xTimeTwo.greatestCommonFactor(this.twoByTwo));
+        assertEquals(isTwo, new Constant(2), this.twoTimeY.greatestCommonFactor(this.twoTimeX));
+        assertEquals(isTwo, new Constant(2), this.twoTimeY.greatestCommonFactor(this.xTimeTwo));
+        assertEquals(isTwo, new Constant(2), this.twoTimeY.greatestCommonFactor(this.twoByTwo));
+        assertEquals(isTwo, new Constant(2), this.twoTimeX.greatestCommonFactor(this.twoTimeY));
+
+        String isThree = "the MCF is constant three";
+        assertEquals(isTwo, new Constant(3), this.threeTimeX.greatestCommonFactor(this.threeTimeY));
+        assertEquals(isTwo, new Constant(3), this.threeTimeY.greatestCommonFactor(this.threeTimeX));
+
+        assertEquals(new Symbol("x"), this.twoTimeX.greatestCommonFactor(this.zeroTimeX));
     }
 
     @Test
