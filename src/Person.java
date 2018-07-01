cbfd008265918a4315b92ab82cc6030bc2ac05f9
daolao <dalao1002@gmail.com>
2018-07-01 18:36:23 +0200

    annotation processing test

---
+++ src/Person.java
@@ -0,0 +1,23 @@
+public class Person {
+    private int age;
+
+    private String name;
+
+    public int getAge() {
+        return age;
+    }
+
+    @BuilderProperty
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @BuilderProperty
+    public void setName(String name) {
+        this.name = name;
+    }
+}
