828724bacfc98b9f64c968bb0dbe41fc1ebe69e9
daolao <dalao1002@gmail.com>
2018-07-14 16:38:18 +0200

    P10107 not solved

--- src/P10107.java
+++ src/P10107.java
@@ -4,6 +4,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Arrays;
 
+// TODO: not solved
 class P10107 {
     public static void main(String[] args) throws Exception {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
