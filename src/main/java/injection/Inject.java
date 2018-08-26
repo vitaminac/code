4c3f09fe853029450bdfaadda71df10f031e7b02
daolao <dalao1002@gmail.com>
2018-08-26 18:35:26 +0200

    add annotation

--- src/main/java/injection/Inject.java
+++ src/main/java/injection/Inject.java
@@ -1,11 +1,15 @@
 package injection;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Target({ElementType.PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@Documented
 public @interface Inject {
 }
