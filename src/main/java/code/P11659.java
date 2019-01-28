31512ed7a0d09629b3dfbd836cc8e67b7879ce52
daolao <dalao1002@gmail.com>
2019-01-28 22:23:31 +0100

    change time unit

--- src/main/java/code/P11659.java
+++ src/main/java/code/P11659.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+// https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=2706
 class P11659 {
     public static void main(String[] args) throws Exception {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
