c710ed146e7600b2a32289ece22af5f697c4e397
daolao <dalao1002@gmail.com>
2019-01-29 13:26:07 +0100

    P10038

--- src/main/java/code/P10038.java
+++ src/main/java/code/P10038.java
@@ -1,37 +1,36 @@
 package code;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 //https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=979
 class P10038 {
-    public static void main(String[] args) throws Exception {
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-            String input;
-            StringTokenizer tokenizer;
-            int before, next, dif, n, i;
-            while (((input = br.readLine()) != null)) {
-                tokenizer = new StringTokenizer(input);
-                n = Integer.parseInt(tokenizer.nextToken());
-                boolean[] bitMap = new boolean[n];
-                before = Integer.parseInt(tokenizer.nextToken());
-                i = n - 1;
-                while (i > 0) {
-                    next = Integer.parseInt(tokenizer.nextToken());
-                    dif = Math.abs(before - next);
-                    if (dif > 0 && dif < n && !bitMap[dif]) {
-                        bitMap[dif] = true;
-                        before = next;
-                        i = i - 1;
-                    } else {
-                        break;
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (scanner.hasNextInt()) {
+                int n = scanner.nextInt();
+                boolean valid = true;
+                if (n > 0) {
+                    int before = scanner.nextInt();
+                    if (n > 1) {
+                        boolean[] bitMap = new boolean[n];
+                        for (int i = n - 1; i > 0; i--) {
+                            int next = scanner.nextInt();
+                            if (valid) {
+                                int dif = Math.abs(before - next);
+                                if (dif > 0 && dif < n && !bitMap[dif]) {
+                                    bitMap[dif] = true;
+                                    before = next;
+                                } else {
+                                    valid = false;
+                                }
+                            }
+                        }
                     }
                 }
-                if (i == 0) {
-                    System.out.print("Jolly\n");
+                if (valid) {
+                    System.out.println("Jolly");
                 } else {
-                    System.out.print("Not jolly\n");
+                    System.out.println("Not jolly");
                 }
             }
         }
