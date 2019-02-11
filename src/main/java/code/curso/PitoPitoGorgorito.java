176ff68f10e2ea1e6d251e20bd33b8a6dbfb6e8b
daolao <dalao1002@gmail.com>
2019-02-11 09:16:44 +0100

    PitoPitoGorgorito

--- src/main/java/code/curso/PitoPitoGorgorito.java
+++ src/main/java/code/curso/PitoPitoGorgorito.java
@@ -2,6 +2,27 @@ package code.curso;
 
 import java.util.Scanner;
 
+/**
+ * ”Pito pito gorgorito donde vas tu tan bonito a la era verdadera pin pan pun fuera t´u te vas y
+ * t´u te quedas”
+ * ”En la casa de pinocho todos cuentan hasta 8. 1 2 3 4 5 6 7 8.”
+ * ”Calcet´ın suerte para mi si no fuera para mi ser´a para ti”
+ * Todos de peque˜no alguna vez hemos utilizado esta forma para determinar quien ”se la hinca” a
+ * jugar a cualquier juego (escondite, pilla pilla, etc). Dada la cantidad de silabas, calcular dado
+ * una posici´on si nos salvamos de hincarla o no.
+ * Por ejemplo si somos 10 personas y la frase tiene 5 silabas, el 5 la hinca. Si somos 4 personas
+ * y la frase tiene 5 silabas la hinca el n´umero 1.
+ *
+ * Entrada
+ * Un n´umero T con el n´umero de casos, luego, la descripci´on de T casos de prueba.
+ * Cada caso contiene tres n´umeros: N (n´umero de silabas de la canci´on) y M (n´umero del
+ * grupo de amigos) K (n´umero de posiciones que nos preguntamos si nos la hincar´ıamos o no).
+ * Posteriormente K lineas definiendo cada posici´on por la que se pregunta.
+ *
+ * Salida
+ * Para cada consulta Ki
+ * , ”SI” si nos la hincamos en dicha posici´on y ”NO” si nos salvamos
+ */
 public class PitoPitoGorgorito {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
