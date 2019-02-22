c9b7fd5b563753937ba7185fb2af55ca575536d4
daolao <dalao1002@gmail.com>
2019-02-22 18:28:42 +0100

    HelloWorld

---
+++ src/main/java/code/curso/HelloWorld.java
@@ -0,0 +1,14 @@
+package code.curso;
+
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        for (int i = 0; i < n; i++) {
+            System.out.println();
+            System.out.print("Hello World!");
+        }
+    }
+}
