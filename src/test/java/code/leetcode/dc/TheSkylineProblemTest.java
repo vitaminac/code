a0d89dbfec7db01ed0857cc27cf3ea3a7b5656a4
daolao <dalao1002@gmail.com>
2019-03-26 23:20:43 +0100

    TheSkylineProblemTest

--- src/test/java/code/leetcode/dc/TheSkylineProblemTest.java
+++ src/test/java/code/leetcode/dc/TheSkylineProblemTest.java
@@ -9,12 +9,13 @@ public class TheSkylineProblemTest {
 
     @Test
     public void getSkyline() {
-        int[][] buildings = new int[25][25];
-        buildings[2][9] = 10;
-        buildings[3][7] = 15;
-        buildings[5][12] = 12;
-        buildings[15][20] = 10;
-        buildings[19][24] = 8;
+        int[][] buildings = new int[][]{
+                {2, 9, 10},
+                {3, 7, 15},
+                {5, 12, 12},
+                {15, 20, 10},
+                {19, 24, 8}
+        };
         System.out.println(this.skyline.getSkyline(buildings));
     }
 }
