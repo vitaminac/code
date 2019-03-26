eecb85fd108f45df0859f9e5d35664171e577759
daolao <dalao1002@gmail.com>
2019-03-26 17:24:43 +0100

    Predict The Winner

--- src/main/java/code/leetcode/minimax/PredictTheWinner.java
+++ src/main/java/code/leetcode/minimax/PredictTheWinner.java
@@ -1,15 +1,15 @@
 package code.leetcode.minimax;
 
 public class PredictTheWinner {
-    private static int max(int[] nums, int left, int right, int turn) {
-        if (left == right) return turn * nums[left];
-        return turn * Math.max( // do maximum when turn is negative
-                turn * (turn * nums[left] + max(nums, left + 1, right, -turn)),
-                turn * (turn * nums[right] + max(nums, left, right - 1, -turn))
-        );
-    }
-
     public boolean PredictTheWinner(int[] nums) {
-        return this.max(nums, 0, nums.length - 1, 1) >= 0;
+        int[] dp = new int[nums.length];
+        for (int len = nums.length; len >= 0; len--) {
+            for (int e = len + 1; e < nums.length; e++) {
+                int a = nums[len] - dp[e];
+                int b = nums[e] - dp[e - 1];
+                dp[e] = Math.max(a, b);
+            }
+        }
+        return dp[nums.length - 1] >= 0;
     }
 }
