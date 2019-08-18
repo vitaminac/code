f1d2ab110cc0dde86675d9667ee568c87959c7fc
dalao <dalao1002@gmail.com>
2019-08-18 23:33:13 +0200

    https://code.google.com/codejam/contest/6304486/dashboard#s=p1

---
+++ competition/cheatsheet.h
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef DEBUG
+#define Dbg(x) cerr << __LINE__ << ":" << #x << "=" << x << endl;
+#else
+#define Dbg(x)
+#endif
+
+const double E = 1e-8;
+const double PI = acos(-1);
+
+#define LOOP(i, N) for (int i = 0; i < N; i++)
---
+++ competition/kickstart2017practiceB.cpp
@@ -0,0 +1,28 @@
+//#define DEBUG
+#include "cheatsheet.h"
+
+// https://code.google.com/codejam/contest/6304486/dashboard#s=p1
+// https://en.wikipedia.org/wiki/Bertrand%27s_ballot_theorem
+int main()
+{
+    ios::sync_with_stdio(false);
+    freopen("B-large-practice.in", "r", stdin);
+    freopen("ans.out", "w", stdout);
+
+    int T;
+    cin >> T;
+
+    LOOP(i, T)
+    {
+        Dbg(i);
+        int N;
+        int M;
+        cin >> N >> M;
+        Dbg(N);
+        Dbg(M);
+        double y = (double)(N - M) / (N + M);
+        cout << "Case #" << i + 1 << ": " << y << endl;
+    }
+
+    return 0;
+}
