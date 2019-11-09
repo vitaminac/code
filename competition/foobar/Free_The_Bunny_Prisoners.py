a9b4970276886804787e2f6ce5e90fcf5afa7105
dalao <dalao1002@gmail.com>
2019-11-09 17:14:45 +0100

    fix error

--- competition/foobar/Free_The_Bunny_Prisoners.py
+++ competition/foobar/Free_The_Bunny_Prisoners.py
@@ -100,8 +100,8 @@ from itertools import combinations
 
 def solution(num_buns, num_required):
     bunny_keyrings = [[] for num in range(num_buns)]
-    n_key_per_bunny = num_buns - num_required + 1
-    for keynum, keyholders in enumerate(combinations(range(num_buns), n_key_per_bunny)):
+    bunnies_per_key = num_buns - num_required + 1
+    for keynum, keyholders in enumerate(combinations(range(num_buns), bunnies_per_key)):
         for index in keyholders:
             bunny_keyrings[index].append(keynum)
 
