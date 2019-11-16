0f57d287d8b7a5b4fcf88f2297746b77d3918db0
dalao <dalao1002@gmail.com>
2019-11-16 20:50:12 +0100

    to bin

--- competition/funcion.c
+++ competition/funcion.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 
 int inverse(int n)
@@ -45,10 +46,29 @@ int suma_lenta(int n1, int n2)
     return suma_lenta(n1 + 1, n2 - 1);
 }
 
+int to_bin(int decimal, char *result)
+{
+    if (decimal < 2)
+    {
+        result[0] = decimal + '0';
+        return 1;
+    }
+    else
+    {
+        int len = to_bin(decimal / 2, result);
+        result[len] = decimal % 2 + '0';
+        return len + 1;
+    }
+}
+
 int main()
 {
+    char result[255];
     imprimir(inverse(-5));
     printf("es amigo: %s\n", (es_amigo(220, 284) ? "true" : "false"));
     printf("suma lenta: %d\n", suma_lenta(5, 9));
+    int len = to_bin(987654321, result);
+    result[len] = '\0';
+    printf("%s\n", result);
     return 0;
 }
