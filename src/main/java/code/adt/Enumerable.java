8ce368512b7670329c5210bfcb04c964eea9a0f0
dalao <dalao1002@gmail.com>
2019-12-11 22:39:43 +0100

    reduce

--- src/main/java/code/adt/Enumerable.java
+++ src/main/java/code/adt/Enumerable.java
@@ -1,7 +1,9 @@
 package code.adt;
 
 import java.util.Iterator;
+import java.util.function.BiFunction;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 import java.util.function.UnaryOperator;
 
 public interface Enumerable<E> extends Iterable<E> {
@@ -9,6 +11,30 @@ public interface Enumerable<E> extends Iterable<E> {
 
     default Enumerable<E> map(UnaryOperator<E> operator) {
         return consumer -> this.enumerate(e -> consumer.accept(operator.apply(e)));
+    }
+
+    abstract class Reducer<E, R> implements Consumer<E>, Supplier<R> {
+        protected R result;
+
+        private Reducer(R initial) {
+            this.result = initial;
+        }
+
+        @Override
+        public R get() {
+            return this.result;
+        }
+    }
+
+    default <R> R reduce(R identity, BiFunction<E, R, R> accumulator) {
+        Reducer<E, R> reducer = new Reducer<E, R>(identity) {
+            @Override
+            public void accept(E e) {
+                this.result = accumulator.apply(e, this.result);
+            }
+        };
+        this.forEach(reducer);
+        return reducer.get();
     }
 
     @Override
