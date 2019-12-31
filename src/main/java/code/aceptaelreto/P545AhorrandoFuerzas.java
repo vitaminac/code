9a7ec1bdc282d180bada09a4884ea3f55efd1bd2
dalao <dalao1002@gmail.com>
2019-12-31 12:51:23 +0100

    Ahorrando fuerzas

---
+++ src/main/java/code/aceptaelreto/P545AhorrandoFuerzas.java
@@ -0,0 +1,21 @@
+package code.aceptaelreto;
+
+import java.util.Scanner;
+
+public class P545AhorrandoFuerzas {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            while (sc.hasNextInt()) {
+                int l = sc.nextInt(), n = sc.nextInt(), min = Integer.MAX_VALUE;
+                for (int i = 0; i < n; i++) {
+                    int t = sc.nextInt();
+                    int v = sc.nextInt();
+                    if (t >= 0) {
+                        min = Math.min(min, t + l / v);
+                    }
+                }
+                System.out.println(min);
+            }
+        }
+    }
+}
