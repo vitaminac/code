cd3b067317c31d4996f1887bd3933f988a7cd22f
dalao <dalao1002@gmail.com>
2019-12-31 17:20:04 +0100

    Que no se atraganten

---
+++ src/main/java/code/aceptaelreto/P544QueNoSeAtraganten.java
@@ -0,0 +1,33 @@
+package code.aceptaelreto;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class P544QueNoSeAtraganten {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            while (sc.hasNextInt()) {
+                int n = sc.nextInt();
+                long p = sc.nextLong();
+                PriorityQueue<Long> pq = new PriorityQueue<>(n, Collections.reverseOrder());
+                for (int i = 0; i < n; i++) {
+                    long uva = sc.nextLong();
+                    if (uva <= p) {
+                        if (pq.isEmpty()) {
+                            pq.add(uva);
+                        } else if (uva + pq.peek() <= p) {
+                            pq.add(uva);
+                        } else if (uva < pq.peek()) {
+                            do {
+                                pq.remove();
+                            } while (!pq.isEmpty() && uva < pq.peek() && pq.peek() + uva > p);
+                            pq.add(uva);
+                        }
+                    }
+                }
+                System.out.println(pq.size());
+            }
+        }
+    }
+}
