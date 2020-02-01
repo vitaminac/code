0cea021857733378dfceaa8a23bab753b79a3c2e
dalao <dalao1002@gmail.com>
2020-02-01 21:50:39 +0100

    https://www.geeksforgeeks.org/n-queen-problem-backtracking-3/

---
+++ src/test/java/code/geekforgeek/backtracking/NQueenTest.java
@@ -0,0 +1,19 @@
+package code.geekforgeek.backtracking;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class NQueenTest {
+    @Test
+    public void queen4() {
+        int[] solution = NQueen.find(4);
+        assertArrayEquals(new int[]{1, 3, 0, 2}, solution);
+    }
+
+    @Test
+    public void queen8() {
+        int[] solution = NQueen.find(8);
+        assertArrayEquals(new int[]{0, 4, 7, 5, 2, 6, 1, 3}, solution);
+    }
+}
