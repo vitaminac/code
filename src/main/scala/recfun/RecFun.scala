86c944ab60af2aa201af8a7e24ce601cd52b014a
dalao <dalao1002@gmail.com>
2020-02-16 00:49:57 +0100

    Recursion in Scala

---
+++ src/main/scala/recfun/RecFun.scala
@@ -0,0 +1,46 @@
+package recfun
+
+object RecFun extends RecFunInterface {
+
+  def main(args: Array[String]): Unit = {
+    println("Pascal's Triangle")
+    for (row <- 0 to 10) {
+      for (col <- 0 to row)
+        print(s"${pascal(col, row)} ")
+      println()
+    }
+  }
+
+  /**
+   * Exercise 1
+   */
+  def pascal(c: Int, r: Int): Int = if (c == 0 || c == r) 1 else pascal(c - 1, r - 1) + pascal(c, r - 1)
+
+  /**
+   * Exercise 2
+   */
+  def balance(chars: List[Char]): Boolean = {
+    def loop(chars: List[Char], open: Int): Boolean = {
+      if (open < 0) false
+      else if (chars.isEmpty) open == 0
+      else if (chars.head == '(') loop(chars.tail, open + 1)
+      else if (chars.head == ')') loop(chars.tail, open - 1)
+      else loop(chars.tail, open)
+    }
+
+    loop(chars, 0)
+  }
+
+  /**
+   * Exercise 3
+   */
+  def countChange(money: Int, coins: List[Int]): Int = {
+    def loop(money: Int, coins: List[Int]): Int = {
+      if (money < 0 || coins.isEmpty) 0
+      else if (money == 0) 1
+      else loop(money, coins.tail) + loop(money - coins.head, coins)
+    }
+
+    loop(money, coins)
+  }
+}
