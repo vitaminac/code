5ceed8e9f0fc37e0ffa59c9a09fe20df46c4fcbf
dalao <dalao1002@gmail.com>
2020-02-16 12:02:50 +0100

    P116

---
+++ src/main/java/code/aceptaelreto/P116HolaMundo.java
@@ -0,0 +1,13 @@
+package code.aceptaelreto;
+
+import java.util.Scanner;
+
+public class P116HolaMundo {
+    public static void main(String[] args) throws Exception {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        for (int i = 0; i < N; i++) {
+            System.out.println("Hola mundo.");
+        }
+    }
+}
