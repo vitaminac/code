3fc855364ec400acfcbfcfdcc138d992f6836080
dalao <dalao1002@gmail.com>
2020-02-18 00:01:11 +0100

    high order function

---
+++ src/main/scala/week2/session.scala
@@ -0,0 +1,11 @@
+package week2
+
+class session {
+  def sum(f: Int => Int, a: Int, b: Int) = {
+    def loop(a: Int, acc: Int): Int =
+      if (a > b) acc
+      else loop(a + 1, f(a) + acc)
+
+    loop(a, 0)
+  }
+}
