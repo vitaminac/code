97878b44b34976f04d8c868733f1f05ecd55265d
dalao <dalao1002@gmail.com>
2020-02-22 17:22:38 +0100

    add TopCoder

---
+++ src/main/java/code/topcoder/ShootingGame.java
@@ -0,0 +1,9 @@
+package code.topcoder;
+
+public class ShootingGame {
+    public double findProbability(int p) {
+        if (p >= 500000) return -1;
+        double q = 1000000 - p;
+        return ((double) p) / q;
+    }
+}
