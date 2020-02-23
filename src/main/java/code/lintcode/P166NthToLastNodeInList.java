80705b7d96c4d6b446cafbc8d180fd5c715bb5ca
dalao <dalao1002@gmail.com>
2020-02-23 21:46:23 +0100

    166. Nth to Last Node in List

---
+++ src/main/java/code/lintcode/P166NthToLastNodeInList.java
@@ -0,0 +1,31 @@
+package code.lintcode;
+
+public class P166NthToLastNodeInList {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+            this.next = null;
+        }
+    }
+
+    /*
+     * @param head: The first node of linked list.
+     * @param n: An integer
+     * @return: Nth to last node of a singly linked list.
+     */
+    public ListNode nthToLast(ListNode slow, int n) {
+        if (slow == null) return null;
+        ListNode fast = slow;
+        for (int i = 1; i < n; i++) {
+            fast = fast.next;
+        }
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+}
