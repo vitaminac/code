2035f357a9695a322b01012a74bb318b26b38f81
dalao <dalao1002@gmail.com>
2020-03-07 23:33:00 +0100

    P212SpaceReplacement 剑指offer Q4

---
+++ src/main/java/code/lintcode/P212SpaceReplacement.java
@@ -0,0 +1,24 @@
+package code.lintcode;
+
+public class P212SpaceReplacement {
+    /*
+     * @param string: An array of Char
+     * @param length: The true length of the string
+     * @return: The true length of new string
+     */
+    public int replaceBlank(char[] string, int length) {
+        int count = 0;
+        for (int i = 0; i < length; i++) if (string[i] == ' ') ++count;
+        int newLength = length + count * 2;
+        for (int i = length - 1, j = newLength - 1; i >= 0; i--) {
+            if (string[i] == ' ') {
+                string[j--] = '0';
+                string[j--] = '2';
+                string[j--] = '%';
+            } else {
+                string[j--] = string[i];
+            }
+        }
+        return newLength;
+    }
+}
