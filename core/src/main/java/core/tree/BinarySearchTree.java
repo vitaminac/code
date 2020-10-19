7532f3b3e5d6794e1c5fef4a130a418e6396983d
dalao <dalao1002@gmail.com>
2020-10-19 20:02:41 +0200

    inline BinaryTree into LinkedBinaryTree

--- core/src/main/java/core/tree/BinarySearchTree.java
+++ core/src/main/java/core/tree/BinarySearchTree.java
@@ -1,6 +1,8 @@
 package core.tree;
 
-public interface BinarySearchTree<E extends Comparable<? super E>, SelfType extends BinarySearchTree<E, SelfType>> extends BinaryTree<E, SelfType> {
+import core.Enumerable;
+
+public interface BinarySearchTree<E extends Comparable<? super E>, SelfType extends BinarySearchTree<E, SelfType>> extends Tree<E, SelfType> {
     default SelfType search(E key) {
         int diff = key.compareTo(this.getElement());
         if (diff == 0) return (SelfType) this;
@@ -11,5 +13,53 @@ public interface BinarySearchTree<E extends Comparable<? super E>, SelfType exte
             if (this.right() == null) return (SelfType) this;
             else return this.right().search(key);
         }
+    }
+
+    SelfType left();
+
+    SelfType right();
+
+    @Override
+    default int height() {
+        return Math.max(this.left() == null ? 0 : this.left().height(), this.right() == null ? 0 : this.right().height()) + 1;
+    }
+
+    @Override
+    default int size() {
+        int size = 1;
+        if (this.left() != null) size += this.left().size();
+        if (this.right() != null) size += this.right().size();
+        return size;
+    }
+
+    @Override
+    default boolean isLeaf() {
+        return this.left() == null && this.right() == null;
+    }
+
+    @Override
+    default Enumerable<SelfType> preOrder() {
+        return consumer -> {
+            consumer.accept((SelfType) this);
+            if (this.left() != null) this.left().preOrder().forEach(consumer);
+            if (this.right() != null) this.right().preOrder().forEach(consumer);
+        };
+    }
+
+    @Override
+    default Enumerable<SelfType> postOrder() {
+        return consumer -> {
+            if (this.left() != null) this.left().postOrder().forEach(consumer);
+            if (this.right() != null) this.right().postOrder().forEach(consumer);
+            consumer.accept((SelfType) this);
+        };
+    }
+
+    default Enumerable<SelfType> inOrder() {
+        return consumer -> {
+            if (this.left() != null) this.left().inOrder().forEach(consumer);
+            consumer.accept((SelfType) this);
+            if (this.right() != null) this.right().inOrder().forEach(consumer);
+        };
     }
 }
--- core/src/main/java/core/tree/LinkedBinaryTree.java
+++ core/src/main/java/core/tree/LinkedBinaryTree.java
@@ -1,6 +1,8 @@
 package core.tree;
 
-public class LinkedBinaryTree<E> implements BinaryTree<E, LinkedBinaryTree<E>> {
+import core.Enumerable;
+
+public class LinkedBinaryTree<E> implements Tree<E, LinkedBinaryTree<E>> {
     private E element;
     private LinkedBinaryTree<E> left, right;
 
@@ -8,24 +10,12 @@ public class LinkedBinaryTree<E> implements BinaryTree<E, LinkedBinaryTree<E>> {
         this.element = element;
     }
 
-    @Override
     public LinkedBinaryTree<E> left() {
         return this.left;
     }
 
-    @Override
     public LinkedBinaryTree<E> right() {
         return right;
-    }
-
-    @Override
-    public void left(LinkedBinaryTree<E> tree) {
-        this.left = tree;
-    }
-
-    @Override
-    public void right(LinkedBinaryTree<E> tree) {
-        this.right = tree;
     }
 
     @Override
@@ -36,5 +26,49 @@ public class LinkedBinaryTree<E> implements BinaryTree<E, LinkedBinaryTree<E>> {
     @Override
     public void replace(E element) {
         this.element = element;
+    }
+
+    @Override
+    public int height() {
+        return Math.max(this.left() == null ? 0 : this.left().height(), this.right() == null ? 0 : this.right().height()) + 1;
+    }
+
+    @Override
+    public int size() {
+        int size = 1;
+        if (this.left() != null) size += this.left().size();
+        if (this.right() != null) size += this.right().size();
+        return size;
+    }
+
+    @Override
+    public boolean isLeaf() {
+        return this.left() == null && this.right() == null;
+    }
+
+    @Override
+    public Enumerable<LinkedBinaryTree<E>> preOrder() {
+        return consumer -> {
+            consumer.accept(this);
+            if (this.left() != null) this.left().preOrder().forEach(consumer);
+            if (this.right() != null) this.right().preOrder().forEach(consumer);
+        };
+    }
+
+    @Override
+    public Enumerable<LinkedBinaryTree<E>> postOrder() {
+        return consumer -> {
+            if (this.left() != null) this.left().postOrder().forEach(consumer);
+            if (this.right() != null) this.right().postOrder().forEach(consumer);
+            consumer.accept(this);
+        };
+    }
+
+    public Enumerable<LinkedBinaryTree<E>> inOrder() {
+        return consumer -> {
+            if (this.left() != null) this.left().inOrder().forEach(consumer);
+            consumer.accept(this);
+            if (this.right() != null) this.right().inOrder().forEach(consumer);
+        };
     }
 }
