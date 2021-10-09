6a4e3b76e0cddc58248e6f910490bd88b5ff097c
dalao <dalao1002@gmail.com>
2021-10-09 17:30:54 +0200

    Stack

---
+++ judge/src/main/java/algs4/fundamentals/stacks/example/Stack.java
@@ -0,0 +1,21 @@
+package algs4.fundamentals.stacks.example;
+
+import algs4.stdlib.StdIn;
+import algs4.stdlib.StdOut;
+
+/**
+ * https://algs4.cs.princeton.edu/13stacks/Stack.java.html
+ */
+public class Stack {
+    public static void main(String[] args) {
+        core.stack.Stack<String> stack = core.stack.Stack.fromSinglyLinkedListDoubleReference();
+        while (!StdIn.isEmpty()) {
+            String item = StdIn.readString();
+            if (!item.equals("-"))
+                stack.push(item);
+            else if (!stack.isEmpty())
+                StdOut.print(stack.pop() + " ");
+        }
+        StdOut.println("(" + stack.size() + " left on stack)");
+    }
+}
