a718d437064aaedb73ee673a073bdb3fe1b8066d
dalao <dalao1002@gmail.com>
2021-10-10 14:56:26 +0200

    Ex10InfixToPostfix

---
+++ judge/src/main/java/algs4/fundamentals/stacks/exercises/Ex10InfixToPostfix.java
@@ -0,0 +1,27 @@
+package algs4.fundamentals.stacks.exercises;
+
+import java.util.StringTokenizer;
+
+import algs4.stdlib.StdIn;
+import algs4.stdlib.StdOut;
+import core.stack.Stack;
+
+/**
+ * https://algs4.cs.princeton.edu/13stacks/InfixToPostfix.java.html
+ */
+public class Ex10InfixToPostfix {
+    public static void main(String[] args) {
+        Stack<String> stack = Stack.fromSinglyLinkedListDoubleReference();
+        while (!StdIn.isEmpty()) {
+            final var tokenizer = new StringTokenizer(StdIn.readLine());
+            while (tokenizer.hasMoreElements()) {
+                final var token = tokenizer.nextToken();
+                if (token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/")) stack.push(token);
+                else if (token.equals(")")) StdOut.print(stack.pop() + " ");
+                else if (token.equals("(")) ;
+                else StdOut.print(token + " ");
+            }
+            StdOut.println();
+        }
+    }
+}
--- judge/src/test/java/JudgeSingleTest.java
+++ judge/src/test/java/JudgeSingleTest.java
@@ -1,4 +1,4 @@
-import algs4.fundamentals.stacks.web.WebEx15Infix2Postfix;
+import algs4.fundamentals.stacks.exercises.Ex10InfixToPostfix;
 import test.TestCase;
 import test.TestSuite;
 import test.UnitTest;
@@ -7,6 +7,6 @@ import test.UnitTest;
 public class JudgeSingleTest extends TestCase {
     @UnitTest
     public void test() throws Exception {
-        Judge.judge(WebEx15Infix2Postfix.class, null);
+        Judge.judge(Ex10InfixToPostfix.class, null);
     }
 }
