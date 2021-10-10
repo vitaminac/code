44fda175ea139d7144bd6ad9d97a44fad7acfa48
dalao <dalao1002@gmail.com>
2021-10-10 18:39:39 +0200

    CreativeProblem45VerifyIntermixedStackOperationSequence

--- judge/src/main/java/algs4/fundamentals/stacks/creative/CreativeProblem45VerifyIntermixedStackOperationSequence.java
+++ judge/src/main/java/algs4/fundamentals/stacks/creative/CreativeProblem45VerifyIntermixedStackOperationSequence.java
@@ -10,12 +10,13 @@ public class CreativeProblem45VerifyIntermixedStackOperationSequence {
     public static void main(String[] args) {
         try (final var sc = new Scanner(System.in)) {
             while (sc.hasNextInt()) {
+                int N = sc.nextInt();
                 final var queue = Queue.<Integer>fromSteque(SinglyLinkedListDoubleReference::new);
                 final var stack = Stack.<Integer>fromSteque(SinglyLinkedListDoubleReference::new);
-                for (int i = 0; i <= 9; i++) {
+                for (int i = 0; i < N; i++) {
                     queue.enqueue(sc.nextInt());
                 }
-                for (int i = 0; i <= 9; i++) {
+                for (int i = 0; i < N; i++) {
                     stack.push(i);
                     while (!stack.isEmpty() && stack.peek().equals(queue.peek())) {
                         stack.pop();
