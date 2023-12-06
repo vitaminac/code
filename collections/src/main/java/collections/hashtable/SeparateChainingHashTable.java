64ff8b2fa01ee6249aa08d52f71827a33c03bbec
vitaminac <dalao1002@gmail.com>
2023-12-06 23:01:55 +0100

    refactor SeparateChainingHashTable to use SinglyLinkedListNode

--- collections/src/main/java/collections/hashtable/SeparateChainingHashTable.java
+++ collections/src/main/java/collections/hashtable/SeparateChainingHashTable.java
@@ -1,35 +1,27 @@
 package collections.hashtable;
 
+import collections.linkedlist.SinglyLinkedListNode;
 import collections.map.Relation;
 
 import java.util.function.Consumer;
 
-public class SeparateChainingHashTable<Key, Value> extends AbstractHashTable<Key, Value, SeparateChainingHashTable.Entry<Key, Value>> {
-    public static class Entry<Key, Value> {
-        private final Relation<Key, Value> value;
-        private Entry<Key, Value> next;
-
-        public Entry(final Key key, final Value value) {
-            this.value = new Relation<>(key, value);
-        }
-    }
-
+public class SeparateChainingHashTable<Key, Value> extends AbstractHashTable<Key, Value, SinglyLinkedListNode<Relation<Key, Value>>> {
     @Override
     public void put(final Key key, final Value value) {
         int bucketIndex = this.compress(this.hash(key));
         var current = this.entries[bucketIndex];
         if (current == null) {
             addNewEntry(key, value, bucketIndex);
-        } else if (current.value.getKey().equals(key)) {
-            this.entries[bucketIndex] = new Entry<>(key, value);
+        } else if (current.getElement().getKey().equals(key)) {
+            this.entries[bucketIndex] = new SinglyLinkedListNode<>(new Relation<>(key, value));
         } else {
             var prev = current;
-            while ((current = current.next) != null) {
-                if (current.value.getKey().equals(key)) {
-                    var next = current.next;
-                    final var newEntry = new Entry<>(key, value);
-                    prev.next = newEntry;
-                    newEntry.next = next;
+            while ((current = current.getNext()) != null) {
+                if (current.getElement().getKey().equals(key)) {
+                    var next = current.getNext();
+                    final var newEntry = new SinglyLinkedListNode<>(new Relation<>(key, value));
+                    prev.setNext(newEntry);
+                    newEntry.setNext(next);
                     return;
                 }
                 prev = current;
@@ -39,8 +31,8 @@ public class SeparateChainingHashTable<Key, Value> extends AbstractHashTable<Key
     }
 
     private void addNewEntry(final Key key, final Value value, final int bucketIndex) {
-        var newEntry = new Entry<>(key, value);
-        newEntry.next = this.entries[bucketIndex];
+        var newEntry = new SinglyLinkedListNode<>(new Relation<>(key, value));
+        newEntry.setNext(this.entries[bucketIndex]);
         this.entries[bucketIndex] = newEntry;
         this.size += 1;
         if (this.size > this.capacity * LOAD_FACTOR) this.resize(this.capacity * 2);
@@ -52,10 +44,10 @@ public class SeparateChainingHashTable<Key, Value> extends AbstractHashTable<Key
         if (this.entries[index] != null) {
             var current = this.entries[index];
             while (current != null) {
-                if (current.value.getKey().equals(key)) {
-                    return current.value.getValue();
+                if (current.getElement().getKey().equals(key)) {
+                    return current.getElement().getValue();
                 }
-                current = current.next;
+                current = current.getNext();
             }
         }
         return null;
@@ -66,17 +58,17 @@ public class SeparateChainingHashTable<Key, Value> extends AbstractHashTable<Key
         int index = this.compress(this.hash(key));
         var current = this.entries[index];
         if (current != null) {
-            if (current.value.getKey().equals(key)) {
-                this.entries[index] = current.next;
+            if (current.getElement().getKey().equals(key)) {
+                this.entries[index] = current.getNext();
                 this.size -= 1;
             } else {
-                while (current.next != null) {
-                    if (current.next.value.getKey().equals(key)) {
-                        current.next = current.next.next;
+                while (current.getNext() != null) {
+                    if (current.getNext().getElement().getKey().equals(key)) {
+                        current.setNext(current.getNext().getNext());
                         this.size -= 1;
                         break;
                     }
-                    current = current.next;
+                    current = current.getNext();
                 }
             }
         }
@@ -86,10 +78,10 @@ public class SeparateChainingHashTable<Key, Value> extends AbstractHashTable<Key
 
     @Override
     public void enumerate(Consumer<? super Key> consumer) {
-        for (Entry<Key, Value> entry : this.entries) {
+        for (SinglyLinkedListNode<Relation<Key, Value>> entry : this.entries) {
             while (entry != null) {
-                consumer.accept(entry.value.getKey());
-                entry = entry.next;
+                consumer.accept(entry.getElement().getKey());
+                entry = entry.getNext();
             }
         }
     }
@@ -99,7 +91,7 @@ public class SeparateChainingHashTable<Key, Value> extends AbstractHashTable<Key
     protected void init(int capacity) {
         this.capacity = capacity;
         @SuppressWarnings("unchecked")
-        Entry<Key, Value>[] entries = new Entry[this.capacity];
+        final SinglyLinkedListNode<Relation<Key, Value>>[] entries = new SinglyLinkedListNode[this.capacity];
         this.entries = entries;
         this.size = 0;
     }
@@ -109,8 +101,8 @@ public class SeparateChainingHashTable<Key, Value> extends AbstractHashTable<Key
         this.init(capacity);
         for (var current : old) {
             while (current != null) {
-                this.put(current.value.getKey(), current.value.getValue());
-                current = current.next;
+                this.put(current.getElement().getKey(), current.getElement().getValue());
+                current = current.getNext();
             }
         }
     }
