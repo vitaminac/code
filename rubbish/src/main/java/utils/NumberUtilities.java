a54869f606fbbd7cb60964f54c46cc12dd916cff
vitaminac <dalao1002@gmail.com>
2024-12-11 18:15:59 +0100

    removal of deprecate new Double/Float/Long/Integer

--- rubbish/src/main/java/utils/NumberUtilities.java
+++ rubbish/src/main/java/utils/NumberUtilities.java
@@ -2,43 +2,39 @@ package utils;
 
 // TODO: fixed
 public class NumberUtilities {
+    public static Number addNumbers(final Number a, final Number b) {
+        if (a instanceof Double || b instanceof Double) {
+            return a.doubleValue() + b.doubleValue();
+        } else if (a instanceof Float || b instanceof Float) {
+            return a.floatValue() + b.floatValue();
+        } else if (a instanceof Long || b instanceof Long) {
+            return a.longValue() + b.longValue();
+        } else {
+            return a.intValue() + b.intValue();
+        }
+    }
 
-	public static Number addNumbers(Number a, Number b) {
-		if (a instanceof Double || b instanceof Double) {
-			return new Double(a.doubleValue() + b.doubleValue());
-		} else if (a instanceof Float || b instanceof Float) {
-			return new Float(a.floatValue() + b.floatValue());
-		} else if (a instanceof Long || b instanceof Long) {
-			return new Long(a.longValue() + b.longValue());
-		} else {
-			return new Integer(a.intValue() + b.intValue());
-		}
-	}
+    public static Number subNumbers(Number a, Number b) {
+        if (a instanceof Double || b instanceof Double) {
+            return a.doubleValue() - b.doubleValue();
+        } else if (a instanceof Float || b instanceof Float) {
+            return a.floatValue() - b.floatValue();
+        } else if (a instanceof Long || b instanceof Long) {
+            return a.longValue() - b.longValue();
+        } else {
+            return a.intValue() - b.intValue();
+        }
+    }
 
-	public static Number subNumbers(Number a, Number b) {
-		if (a instanceof Double || b instanceof Double) {
-			return new Double(a.doubleValue() - b.doubleValue());
-		} else if (a instanceof Float || b instanceof Float) {
-			return new Float(a.floatValue() - b.floatValue());
-		} else if (a instanceof Long || b instanceof Long) {
-			return new Long(a.longValue() - b.longValue());
-		} else {
-			return new Integer(a.intValue() - b.intValue());
-		}
-	}
-
-	public static Number multNumbers(Number a, Number b) {
-		if (a instanceof Double || b instanceof Double) {
-			return new Double(a.doubleValue() * b.doubleValue());
-		} else if (a instanceof Float || b instanceof Float) {
-			return new Float(a.floatValue() * b.floatValue());
-		} else if (a instanceof Long || b instanceof Long) {
-			return new Long(a.longValue() * b.longValue());
-		} else {
-			return new Integer(a.intValue() * b.intValue());
-		}
-	}
-
-	
-	
+    public static Number multNumbers(Number a, Number b) {
+        if (a instanceof Double || b instanceof Double) {
+            return a.doubleValue() * b.doubleValue();
+        } else if (a instanceof Float || b instanceof Float) {
+            return a.floatValue() * b.floatValue();
+        } else if (a instanceof Long || b instanceof Long) {
+            return a.longValue() * b.longValue();
+        } else {
+            return a.intValue() * b.intValue();
+        }
+    }
 }
