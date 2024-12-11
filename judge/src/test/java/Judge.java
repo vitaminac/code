9c7ca5d3cbf4a646e1242345799d2f9c3081c32a
vitaminac <dalao1002@gmail.com>
2024-12-11 19:15:22 +0100

    remove noisy stacktrace

--- judge/src/test/java/Judge.java
+++ judge/src/test/java/Judge.java
@@ -17,7 +17,7 @@ import static org.junit.Assert.*;
 
 public final class Judge {
     private static <T> void judge(Class<T> clazz, Method method, Object instance, Object params, Path input, Path output)
-            throws Exception {
+        throws Exception {
         // map rid of platform depend new line
         System.setProperty("line.separator", "\n");
 
@@ -54,46 +54,58 @@ public final class Judge {
 
         // report
         System.out.printf("Time elapsed for %s with input %s and output %s: %d microsecond\n",
-                clazz.getSimpleName(),
-                input.getFileName(),
-                output.getFileName().toString(),
-                elapsedTime / 1000);
+            clazz.getSimpleName(),
+            input.getFileName(),
+            output.getFileName().toString(),
+            elapsedTime / 1000);
     }
 
     public static <T> void judge(Class<T> clazz, String[] params) throws Exception {
         if (clazz.getEnclosingClass() != null ||
-                Modifier.isStatic(clazz.getModifiers())) {
+            Modifier.isStatic(clazz.getModifiers())) {
             Utils.warn(clazz.getName() + " skipped, inner class or lambda");
-        } else {
-            Method main = clazz.getMethod("main", String[].class);
-            if (Modifier.isPrivate(clazz.getModifiers())) {
-                Utils.warn(clazz.getName() + " skipped, no main exist or class is hidden!");
-            } else {
-                int count = 0;
-                final URL testResource = clazz.getResource(clazz.getSimpleName());
-                if (testResource != null) {
-                    Path testSourcePath = Paths.get(testResource.toURI());
-                    File testSourceFile = testSourcePath.toFile();
-                    if (testSourceFile.exists() && testSourceFile.isDirectory()) {
-                        List<Path> inputs = Files.list(testSourcePath).collect(Collectors.toList());
-                        assertNotEquals(clazz.getSimpleName() + "'s input is empty", 0, inputs.size());
-                        for (Path input : inputs) {
-                            if (input.toString().endsWith(".in")) {
-                                String filename = input.getFileName().toString();
-                                judge(clazz, main, null, params, input, testSourcePath.resolve(filename.substring(0, filename.length() - 3) + ".out"));
-                                count += 1;
-                            }
-                        }
-                    }
-                }
-                final URL inputResource = clazz.getResource(clazz.getSimpleName() + ".in");
-                final URL outputResource = clazz.getResource(clazz.getSimpleName() + ".out");
-                if (inputResource != null && outputResource != null) {
-                    judge(clazz, main, null, null, Paths.get(inputResource.toURI()), Paths.get(outputResource.toURI()));
+            return;
+        }
+
+        if (Modifier.isPrivate(clazz.getModifiers())) {
+            Utils.warn(clazz.getName() + " skipped, class is hidden!");
+            return;
+        }
+
+        final Method main;
+        try {
+            main = clazz.getMethod("main", String[].class);
+        } catch (NoSuchMethodException e) {
+            Utils.warn(clazz.getName() + " skipped, no main exist!");
+            return;
+        }
+
+        int count = 0;
+        final URL testResource = clazz.getResource(clazz.getSimpleName());
+        if (testResource == null) {
+            Utils.warn(clazz.getName() + " skipped, test resource is empty!");
+        }
+
+        Path testSourcePath = Paths.get(testResource.toURI());
+        File testSourceFile = testSourcePath.toFile();
+        if (testSourceFile.exists() && testSourceFile.isDirectory()) {
+            final List<Path> inputs = Files.list(testSourcePath).toList();
+            assertNotEquals(clazz.getSimpleName() + "'s input is empty", 0, inputs.size());
+            for (Path input : inputs) {
+                if (input.toString().endsWith(".in")) {
+                    String filename = input.getFileName().toString();
+                    judge(clazz, main, null, params, input, testSourcePath.resolve(filename.substring(0, filename.length() - 3) + ".out"));
                     count += 1;
                 }
-                assertTrue(clazz.getSimpleName() + " hasn't been tested! No input found!", count > 0);
             }
         }
+
+        final URL inputResource = clazz.getResource(clazz.getSimpleName() + ".in");
+        final URL outputResource = clazz.getResource(clazz.getSimpleName() + ".out");
+        if (inputResource != null && outputResource != null) {
+            judge(clazz, main, null, null, Paths.get(inputResource.toURI()), Paths.get(outputResource.toURI()));
+            count += 1;
+        }
+        assertTrue(clazz.getSimpleName() + " hasn't been tested! No input found!", count > 0);
     }
 }
